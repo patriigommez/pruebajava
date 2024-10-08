ec601daec32002ca2064193a8526e66f2024a9e3
patriigommez <pgomez@steam.thehubfp.es>
2024-10-08 12:02:37 +0000

    reto2

---
+++ reto2.java
@@ -0,0 +1,22 @@
+public class reto2 {
+	public static void main(String[] args) {
+		//Pedir nombre
+		
+		String nombre = "Ana";
+		String apellidos = "Lisa Melano";
+	
+		
+		String saludo = ("Hola, " + nombre + apellidos);
+		String cargo = "Secretaria" ;
+		
+		System.out.println(nombre.toUpperCase());
+		
+		apellidos.indexOf(" ");
+		System.out.println(apellidos.indexOf(" "));
+		
+		System.out.println(apellidos.substring(4));
+		
+		System.out.println(apellidos.toUpperCase());
+		
+	}	
+}
