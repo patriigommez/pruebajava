c952a9d0559d21b5b99196e27d0f808a577a889a
patriigommez <pgomez@steam.thehubfp.es>
2024-11-01 11:30:34 +0100

    reto4.java

--- reto4.java
+++ reto4.java
@@ -1,5 +1,18 @@
+import java.util.Random;
 import java.util.Scanner;
 public class Reto4 {
 	public static void main(String[] args) {
+		String cartas [] = {"Reina de corazones","Dos de picas","As de diamantes"};
+		Random rd = new Random();
+        	Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Cartas iniciales: " + cartas);
+		
+		
+
+
+
+
+		
     {
       {
